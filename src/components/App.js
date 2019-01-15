dae452af6826d2116452cc8c49ace48f3ece50d3
Vincent Rohde <vincent.rohde@icloud.com>
2019-01-15 15:43:06 +0100

    add Example component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import './App.scss';
 
+import Example from './Example/Example';
+
 export default class App extends Component {
     render() {
         return (
-            <div className="App">Hello React.</div>
+            <div className="App">
+                <Example />
+            </div>
         );
     }
 }
---
+++ src/components/Example/Example.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Example.scss';
+
+// try to use stateless components whenever possible
+// for more info see: https://programmingwithmosh.com/react/react-functional-components/
+
+const Example = () => {
+    return (
+        <div className="Example">Hello React.</div>
+    );
+}
+
+export default Example;
