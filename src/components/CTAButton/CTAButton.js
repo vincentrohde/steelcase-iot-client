8c23705663d0ced03a96b1a14d47daf852bbce99
Momaritti <marietheres.monz@gmail.com>
2019-02-02 15:32:56 +0100

    change  app.js

---
+++ src/components/CTAButton/CTAButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './CTAButton.scss';
+
+// this is the component for the call to action Button
+
+const CTAButton = () => {
+    return (
+        <div className="CTAButton">Das ist ein call to action button.</div>
+    );
+}
+export default CTAButton;
