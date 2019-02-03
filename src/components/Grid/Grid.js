9aedd082d7bd07fed41424d8a6f0c1bcb092db36
Vincent Rohde <vincent.rohde@icloud.com>
2019-02-03 18:02:32 +0100

    add basic styling, Grid component

---
+++ src/components/Grid/Grid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Grid.scss';
+
+const Grid = () => {
+    return (
+        <div className="Grid">
+            This is the grid
+        </div>
+    );
+}
+
+export default Grid;
--- src/routes/CreateOrder/CreateOrder.js
+++ src/routes/CreateOrder/CreateOrder.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 
+import Grid from '../../components/Grid/Grid';
 import CTAButton from '../../components/CTAButton/CTAButton';
+
 import './CreateOrder.scss';
 
 class CreateOrder extends Component {
     constructor(props) {
         super();
         this.probs = props;
+        this.title = "Anordnung benennen";
     }
 
     render() {
         return (
             <div className="Page CreateOrder">
-                CreateOrder
+                <input
+                    className="order-name"
+                    type="text"
+                    id="name"
+                    name="name"
+                    required
+                    placeholder={this.title} />
+                <Grid />
                 <div className="button-container">
                     <CTAButton />
                 </div>
