78b7044b233d8888d0bf399d24c55c0a20a6af8a
Momaritti <marietheres.monz@gmail.com>
2019-02-08 21:52:09 +0100

    add props to Headline, Lane and AddButton

--- src/components/Lane/Lane.js
+++ src/components/Lane/Lane.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Rooms from '../../components/Rooms/Rooms';
 import './Lane.scss';
 
-const Lane = () => {
+const Lane = (props) => {
+    const title = props.title || 'Raumüberischt';
+
     return (
         <div className="Lane">
-            <h2 className="lane-title">This a label</h2>
+            <h2 className="lane-title">{title}</h2>
             <Rooms />
         </div>
     );
