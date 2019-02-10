1acf09dc3352b0f3a3e67c82587e0d5b27dca87a
Momaritti <marietheres.monz@gmail.com>
2019-02-10 20:52:16 +0100

    change AvatarIcon to background image

--- src/components/AvatarIcon/AvatarIcon.js
+++ src/components/AvatarIcon/AvatarIcon.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './AvatarIcon.scss';
-const avatar = require('../../assets/images/avatar.png');
 
 // this is the Avatar icon TO-DO make add Icon and menu function
 
-const AvatarIcon = () => {
+const AvatarIcon = (props) => {
+
+    const LinkTo = props.link || '/';
+
     return (
-      <div className="AvatarIcon">
-          <img className="icon" src={avatar}/>
-      </div>
+      <link className="AvatarIcon" to={LinkTo}></link>
     );
 }
 
