fbe80a5fb7f5b4753ecac9ab76fef0f420b490c0
nastasios <nastasios@hotmail.de>
2019-02-12 15:37:33 +0100

    Linked the Dashboard with the RoomOverview page

--- src/components/Lane/Lane.js
+++ src/components/Lane/Lane.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Rooms from '../../components/Rooms/Rooms';
 import './Lane.scss';
+import {Link} from "react-router-dom";
 
 const Lane = (props) => {
-    //const title = props.title || 'Räume';
+    const title = props.title || '';
+    const linkTo = props.link || '/roomoverview';
+
     //const { Room } = props.information;
 
     return (
         <div className="Lane">
-            <Rooms />
+            <Link className="Lane-Link" to={linkTo}>{title}
+            <Rooms/>
+            </Link>
         </div>
     )
 };
