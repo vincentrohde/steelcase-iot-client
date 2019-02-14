1d85189a69fb83a3bcb965aa1c68726424938859
Vincent Rohde <vincent.rohde@icloud.com>
2019-02-14 16:21:02 +0100

    add body-parser, implement post request handle

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
 const StateManager = require('./src/StateManager');
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     StateManager.getState(res);
+});
+
+app.post('/', function (req, res) {
+    console.log(req.body);
+    res.send('State saved!');
 });
 
 app.listen(4000, function() {});
