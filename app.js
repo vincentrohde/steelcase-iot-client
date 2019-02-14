06d6424fedc30ec935f145085085ba193832c0e3
Vincent Rohde <vincent.rohde@icloud.com>
2019-02-14 14:26:28 +0100

    add cors, update state.json

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const StateManager = require('./src/StateManager');
+
+app.use(cors());
 
 app.get('/', function(req, res) {
     StateManager.getState(res);
 });
 
-const server = app.listen(3000, function() {
-    const port = server.address().port;
-    console.log('Example app listening at http://localhost:%s', port);
-});
+app.listen(4000, function() {});
