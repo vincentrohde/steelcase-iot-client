4ebf91187592b6b9a5af0a510f689f7bb7051c13
Vincent Rohde <vincent.rohde@icloud.com>
2019-02-14 13:54:54 +0100

    get state from server on mounting

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import axios from 'axios';
-import { Provider } from "../stores/Store";
+import { connect, actions } from "../stores/Store";
 
 import DashboardPage from '../routes/Dashboard/Dashboard';
 import CreateOrderPage from '../routes/CreateOrder/CreateOrder';
@@ -10,27 +10,45 @@ import RoomOverview from "../routes/RoomOverview/RoomOverview";
 
 class AppRouter extends Component {
 
+    constructor(props) {
+        super();
+        this.props = props;
+    }
+
     state = {
-        isFirstTime: false
+        isFirstTime: false,
+        server: 'http://localhost:4000'
+    }
+
+    getStateFromServer() {
+        const { server } = this.state;
+        axios.get(server)
+            .then(res => {
+                const { data } = res;
+                actions.addState(data);
+            });
+    }
+
+    componentWillMount() {
+        this.getStateFromServer();
     }
 
     render() {
+
         return (
-            <Provider>
-                <BrowserRouter>
-                    <Switch>
-                        <Route exact path="/" render={()=>(
-                            this.state.isFirstTime ? <Redirect to="/intro" /> : <DashboardPage />
-                        )} />
-                        <Route exact path="/intro" component={IntroPage} />
-                        <Route exact path="/create" component={CreateOrderPage} />
-                        <Route exact path="/room/:id" component={RoomOverview} />
-                        <Redirect to="/" />
-                    </Switch>
-                </BrowserRouter>
-            </Provider>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" render={()=>(
+                        this.state.isFirstTime ? <Redirect to="/intro" /> : <DashboardPage />
+                    )} />
+                    <Route exact path="/intro" component={IntroPage} />
+                    <Route exact path="/create" component={CreateOrderPage} />
+                    <Route exact path="/room/:id" component={RoomOverview} />
+                    <Redirect to="/" />
+                </Switch>
+            </BrowserRouter>
         );
     }
 }
 
-export default AppRouter;
+export default connect(state => ({ context: state }))(AppRouter);
--- src/stores/Store.js
+++ src/stores/Store.js
@@ -1,16 +1,35 @@
 import createStore from "react-waterfall";
 
-import chairs from './Chairs';
-import orders from './Orders';
-import rooms from './Rooms';
-
 const store = {
     initialState: {
-        chairs,
-        orders,
-        rooms
+        orders: [],
+        rooms: [
+            {
+                id: 501,
+                name: 'Sport',
+                orders: [1, 2, 3]
+            }
+        ]
     },
     actionsCreators: {
+        addState: (prevState, actions, newState) => {
+            const newOrders = newState.orders;
+            const newRooms = newState.rooms;
+
+            // let updatedState = {...prevState};
+            // const properties = Object.keys(newState);
+            // properties.forEach(property => {
+            //     property = newState[property];
+            //     updatedState = {...updatedState, newState[property]};
+            // });
+
+            // TODO: Dynamically generate state
+            return {
+                ...prevState,
+                orders: newOrders,
+                rooms: newRooms
+            }
+        },
         addRoom: (prevState, actions, newRoom) => {
             const { rooms } = prevState;
             const newValue = [...rooms, newRoom];
