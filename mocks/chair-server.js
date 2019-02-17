265bd5e284676acd1aa0c141f0d9b65a3f72b7ff
Vincent Rohde <vincent.rohde@icloud.com>
2019-02-17 14:35:18 +0100

    add yRange to chair-server

--- mocks/chair-server.js
+++ mocks/chair-server.js
@@ -2,16 +2,17 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 5000 });
 
-const chairGenerator = (idRange, xRange, bearingRange) => {
+const chairGenerator = (idRange, xRange, yRange, bearingRange) => {
     let chair = {
         id: 0,
         x: 0,
         y: 100,
-        bearing: 90
+        bearing: 0
     }
 
     chair.id = Math.round(Math.random() * (idRange - chair.id) + chair.id);
     chair.x = Math.round(Math.random() * (xRange - chair.x) + chair.x);
+    chair.y = Math.round(Math.random() * (yRange - chair.y) + chair.y);
     chair.bearing = Math.round(Math.random() * (bearingRange - chair.bearing) + chair.bearing);
 
     return chair;
@@ -19,7 +20,7 @@ const chairGenerator = (idRange, xRange, bearingRange) => {
 
 const mockResponse = (socket) => {
     const interval = setInterval(() => {
-        socket.send(JSON.stringify(chairGenerator(3, 200, 120)));
+        socket.send(JSON.stringify(chairGenerator(3, 500, 500, 360)));
     }, 500);
 
     socket.on('close', function() {
