4f274d5a9b3d554c9c468d753925ec8dca9c51af
nastasios <nastasios@hotmail.de>
2019-02-17 21:41:35 +0100

    Styled the Dashboard

--- src/components/Rooms/Room/Room.js
+++ src/components/Rooms/Room/Room.js
@@ -5,11 +5,12 @@ import './Room.scss';
 
 const Room = (props) => {
 
+    const {roomdescription} = "Raum";
     const { id } = props.information || '000';
     const link = `/room/${id}`;
 
     return (
-        <Link to={link} className="Room">{id}</Link>
+        <Link to={link} className="Room"> {id}</Link>
     );
 }
 
